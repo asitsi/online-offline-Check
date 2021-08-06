b96c73c353847d3ac0c75d2c6756d1a23a1fde47
asitsi <41507591+asitsi@users.noreply.github.com>
2021-08-06 18:28:15 +0530

    Update cheak.js

--- cheak.js
+++ cheak.js
@@ -7,11 +7,11 @@ window.addEventListener("online", Online);
 window.addEventListener("offline", offline);
 
 function Online() {
-  doncument.getElenmentById("container").style.background = "green";
+  document.getElementById("container").style.background = "#1b6535";
   document.querySelector("h1").textContent = "you are connected";
 }
 
 function offline() {
-  document.getElementById("container").style.background = "red";
+  document.getElementById("container").style.background = "#d71b3b";
   document.querySelector("h1").textContent = "you are disconnected";
 }
